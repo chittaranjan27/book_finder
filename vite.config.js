62ae765f4798565f43b47dbd8a48d1df0c6e0931
chittaranjan27 <kchittaranjandora@gmail.com>
2025-08-29 00:52:39 +0530

    updated config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "5zxcp4-5173.csb.app",
+      "5173.csb.app",        
+      ".csb.app"             
+    ]
+  }
 })
